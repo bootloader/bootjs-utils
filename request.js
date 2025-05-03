859ab60cfe019205a657f9a9ced19548a95a3313
lalit.tanwar <lalit.tanwar07@gmail.com>
2025-05-03 16:47:05 +0530

    dep update

--- request.js
+++ request.js
@@ -1,37 +1,3 @@
-class Request {
-  constructor(req) {
-    this.req = req;
-  }
-  header(key) {
-    return this.req.header(key);
-  }
-  param(key) {
-    // Set default values
-    let traceId;
-
-    //Check in query params
-    if (traceId === undefined) {
-      traceId = this.req.query?.[key];
-    }
-
-    // Check in cookies
-    if (traceId === undefined) {
-      traceId = this.req.cookies?.[key];
-    }
-    // Check in headers
-    if (traceId === undefined) {
-      traceId = this.req.header(key);
-    }
-
-    return traceId;
-  }
-  headerOrParam(key) {
-    return this.header(key) || this.param(key);
-  }
-
-  static context(req) {
-    return new Request(req);
-  }
-}
+const request = require('@bootloader/context/request');
 // Export a singleton instance
-module.exports = Request;
+module.exports = request;
